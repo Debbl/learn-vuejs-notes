136972617e0cb79249c8fdeae73b6aa543f8fbbc
Debbl <me@aiwan.run>
2022-07-17 22:01:51 +0800

    03_04_组件通信-案例练习

---
+++ 03_learn_component/src/04_组件通信-案例练习/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app">
+    <tab-control :titles="titles" @changeIndexEvent="changePageIndex" />
+    <div>
+      <h2>{{ pageContents[pageIndex] }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import TabControl from './TabControl.vue';
+export default {
+  components: {
+    TabControl,
+  },
+  data() {
+    return {
+      titles: ['衣服', '鞋子', '裤子'],
+      pageContents: ['衣服列表', '鞋子列表', '裤子列表'],
+      pageIndex: 0,
+    };
+  },
+  methods: {
+    changePageIndex(index) {
+      this.pageIndex = index;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ 03_learn_component/src/04_组件通信-案例练习/TabControl.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tab-control">
+    <span
+      v-for="(item, index) of titles"
+      :key="item"
+      @click="changeIndex(index)"
+      :class="{ active: isActiveIndex === index }"
+      >{{ item }}</span
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    titles: {
+      type: Array,
+      required: true,
+    },
+  },
+  emits: ['changeIndexEvent'],
+  data() {
+    return {
+      isActiveIndex: 0,
+    };
+  },
+  methods: {
+    changeIndex(index) {
+      this.isActiveIndex = index;
+      this.$emit('changeIndexEvent', index);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  justify-content: space-evenly;
+  width: 200px;
+  background-color: skyblue;
+}
+.tab-control span {
+  display: inline-block;
+  padding: 0 6px;
+  border: 1px solid red;
+  border-radius: 6px;
+  cursor: pointer;
+}
+.active {
+  color: red;
+}
+</style>
