3ea9bcd7909f7c966011ea7041399a088b865c40
Debbl <me@aiwan.run>
2022-07-18 20:56:13 +0800

    03_06_组件插槽-具名插槽

---
+++ 03_learn_component/src/06_组件插槽-具名插槽/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="app">
+    <nav-bar>
+      <template v-slot:left>
+        <button>按钮</button>
+      </template>
+      <template v-slot:content>
+        <button>按钮2</button>
+      </template>
+      <template v-slot:right>
+        <button>按钮3</button>
+      </template>
+      <template v-slot:default>
+        <a href="#">a link</a>
+      </template>
+    </nav-bar>
+
+    <nav-bar>
+      <template #left>
+        <button>按钮</button>
+      </template>
+      <template #content>
+        <button>按钮2</button>
+      </template>
+      <template #right>
+        <button>按钮3</button>
+      </template>
+      <template #default>
+        <a href="#">a link</a>
+      </template>
+    </nav-bar>
+
+    <nav-bar>
+      <template v-slot:content>
+        <h2>h2h2h2h2</h2>
+      </template>
+    </nav-bar>
+
+    <nav-bar>
+      <template v-slot:[name]>
+        <h2>h2h2h2h2</h2>
+      </template>
+    </nav-bar>
+    <button @click="name = 'left'">left</button>
+    <button @click="name = 'content'">content</button>
+    <button @click="name = 'right'">right</button>
+    <hr />
+
+    <nav-bar>
+      <template #left>
+        <button>{{ leftText }}</button>
+      </template>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from './NavBar.vue';
+export default {
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      message: 'Hello Vue',
+      name: 'right',
+      leftText: 'left-text',
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ 03_learn_component/src/06_组件插槽-具名插槽/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="nav-bar">
+    <div class="left">
+      <slot name="left">left</slot>
+    </div>
+    <div class="content">
+      <slot name="content">content</slot>
+    </div>
+    <div class="right">
+      <slot name="right">right</slot>
+    </div>
+  </div>
+
+  <div class="other">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.nav-bar {
+  display: flex;
+  width: 300px;
+  text-align: center;
+}
+.left {
+  width: 80px;
+  background-color: skyblue;
+}
+.content {
+  flex: 1;
+  background-color: #eee;
+}
+.right {
+  width: 80px;
+  background-color: #ccc;
+}
+</style>
